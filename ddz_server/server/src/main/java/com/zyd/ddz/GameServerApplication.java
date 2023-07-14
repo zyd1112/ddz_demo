31920009ab80b24a698029d6982bc44eb29ee440
zyd <zyd13975606007@163.com>
2023-07-14 20:44:13 +0800

    update

--- ddz_server/server/src/main/java/com/zyd/ddz/GameServerApplication.java
+++ ddz_server/server/src/main/java/com/zyd/ddz/GameServerApplication.java
@@ -6,34 +6,27 @@ import com.zyd.ddz.server.event.RoomHeartEvent;
 import com.zyd.ddz.server.factory.RoomManagerFactory;
 import com.zyd.zgame.GameServerBootstrap;
 import com.zyd.zgame.core.thread.ThreadManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 
 /**
  * @author zyd
  * @date 2022/12/26 12:37
  */
-@SpringBootApplication(scanBasePackageClasses = {GameServerApplication.class, GameServerBootstrap.class})
-@PropertySource(value = "classpath:config.properties", encoding = "UTF-8")
-@ComponentScan(basePackageClasses = {GameServerApplication.class, GameServerBootstrap.class})
+@SpringBootApplication
 public class GameServerApplication {
 
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(GameServerApplication.class, args);
-        context.getBeanFactory().getBean(GameServerBootstrap.class).startUp();
+        context.getBeanFactory().getBean(GameServerBootstrap.class).startUp(context);
 
-        context.registerShutdownHook();
-
-        ThreadManager.getDISPATCHER().dispatcherSchedule(new MailCheckEvent());
+        ThreadManager.getDispatcher().dispatcherSchedule(new MailCheckEvent());
 
         for (AbstractRoomManager roomManager : RoomManagerFactory.getRooms().values()) {
-            ThreadManager.getDISPATCHER().dispatcherSchedule(new RoomHeartEvent(roomManager));
+            ThreadManager.getDispatcher().dispatcherSchedule(new RoomHeartEvent(roomManager));
         }
     }
 }
